98c78863f1314a906becba04580233f8657bf014
Bryan Arendt <barendt@covermymeds.com>
2016-08-19 13:48:36 -0400

    adds test for >>>

--- lib/metro_rail.ex
+++ lib/metro_rail.ex
@@ -1,3 +1,118 @@
 defmodule MetroRail do
-  @moduledoc File.read!("README.md")
+  defstruct id: "This is just here for unit tests."
+
+  # @moduledoc File.read!("README.md")
+  require IEx
+
+  defmacro __using__(_) do
+    quote do
+      import MetroRail
+      require Logger
+
+      @doc ~S"""
+        Returns the struct
+      """
+      def my_struct do
+        unquote(struct_ast(__CALLER__.module))
+      end
+
+      @doc ~S"""
+       Strips the input and callstack from the 4 value tuple.
+      """
+      def return({status, _, _, output}) do
+        {status, output}
+      end
+
+      def return({status, _, callstack, output} = last, :log) do
+        stack_status = get_status(last)
+        log_stack(stack_status, last)
+        {status, output}
+      end
+
+      @doc """
+        If there is anythign in the stack other than :ok the first thing encountered in reverse order will be returned as the status of the entire stack.
+      """
+      defp get_status({:ok, _, nil, _}), do: :ok
+      defp get_status({:ok, _, callstack, _}), do: get_status(callstack)
+      defp get_status({status, input, callstack, struct}), do: status
+
+      defp log_stack(stack_status, {status, input, nil, struct} = x) do
+
+      end
+      defp log_stack(stack_status, {status, input, callstack, struct} = x) do
+        log_stack(stack_status, callstack)
+        log_both(stack_status, callstack, x)
+      end
+
+      defp log_both(:ok, {status, input, _, _}, {_, nil, _, output}) do
+        Logger.debug "#{inspect status} Input: #{inspect input} Output: #{inspect output}"
+      end
+      defp log_both(:ok, {status, input, _, _}, {_, output, _, _}) do
+        Logger.debug "#{inspect status} Input: #{inspect input} Output: #{inspect output}"
+      end
+      defp log_both(:ok, {i_status, i_input, _, i_struct} = x, _) do
+      end
+
+      defp log_both(:error, {status, input, _, _}, {_, nil, _, output}) do
+        Logger.error "#{inspect status} Input: #{inspect input} Output: #{inspect output}"
+      end
+      defp log_both(:error, {status, input, _, _}, {_, output, _, _}) do
+        Logger.error "#{inspect status} Input: #{inspect input} Output: #{inspect output}"
+      end
+      defp log_both(:error, {i_status, i_input, _, i_struct} = x, _) do
+      end
+
+    end
+  end
+
+
+  @doc ~S"""
+     Will return the name of the struct for this MetroRail
+
+     For example if this macro is used in 'RequestService' it will return 'RequestServiceStruct'
+  """
+  def struct_ast(x) do
+    {:%, [],
+     [{:__aliases__, [alias: false],
+      [(x |> Atom.to_string)
+      <> "Struct" |> String.to_atom]}, {:%{}, [], []}]}
+  end
+
+  @doc ~S"""
+    Given a 4 value Metro Tuple where the 4th tuple is the services struct it will pass it along
+
+    Otherwise it will create the 4 value Metro Tuple and use the entire input as the 2nd value
+  """
+  defmacro left >>> right do
+    quote do
+      (fn ->
+        case unquote(left) do
+          { status, input, call_stack, context_struct} ->
+            unquote(left)
+            |> unquote(right)
+
+          input ->
+            {:ok, input, nil, unquote(struct_ast(__CALLER__.module))}
+            |> unquote(right)
+        end
+      end).()
+    end
+  end
+
+  #TODO use bind_quoted if possible. it doesn't work for some reason maybe because we are using the special operator
+  # defmacro left ~>> right do
+  #   quote bind_quoted: [left: left, right: right, struct: struct_ast(__CALLER__.module)] do
+  #     (fn ->
+  #       case left do
+  #         { status, input, call_stack, context_struct} ->
+  #           left
+  #           |> right
+  #
+  #         input ->
+  #           {:ok, input, nil, struct }
+  #           |> right
+  #       end
+  #     end).()
+  #   end
+  # end
 end
--- test/metro_rail_test.exs
+++ test/metro_rail_test.exs
@@ -1,8 +1,26 @@
 defmodule MetroRailTest do
   use ExUnit.Case
   doctest MetroRail
+  require MetroRail
 
-  test "the truth" do
-    assert 1 + 1 == 2
+  test "ensure loaded" do
+    assert Code.ensure_loaded?(MetroRail)
+  end
+
+  test ">>> expands properly" do
+    input_macro = quote do
+      MetroRail.>>>(:x, String.valid?)
+    end |> Macro.expand( __ENV__ ) |> Macro.to_string
+
+    expected_code = quote do
+      fn -> case(:x) do
+        {status, input, call_stack, context_struct} ->
+          :x |> String.valid?()
+        input ->
+          {:ok, input, nil,  %Elixir.MetroRailTestStruct{} } |> String.valid?()
+      end end.()
+    end |> Macro.to_string
+
+    assert input_macro == expected_code
   end
 end
